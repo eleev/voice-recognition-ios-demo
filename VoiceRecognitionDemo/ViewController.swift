430cac4befdb7ef2171a570f3b154eb4a551dbae
Astemir Eleev <astemireleev@gmail.com>
2016-08-21 12:30:05 +0300

    Initial Commit

---
+++ VoiceRecognitionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoiceRecognitionDemo
+//
+//  Created by Astemir Eleev on 21/08/16.
+//  Copyright © 2016 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
